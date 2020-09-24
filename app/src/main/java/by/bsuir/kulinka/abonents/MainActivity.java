bf1325b95c32bab904261f7fb103b7947c734e55
SergeantBulkin <bezdeneg.ya@yandex.ru>
2020-09-24 19:33:54 +0300

    Better way to BottomNavigation

--- app/src/main/java/by/bsuir/kulinka/abonents/MainActivity.java
+++ app/src/main/java/by/bsuir/kulinka/abonents/MainActivity.java
@@ -1,26 +1,37 @@
 package by.bsuir.kulinka.abonents;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+import android.widget.Toolbar;
 
 import com.bsuir.bottomapp.bar.abonents.R;
-
-import by.bsuir.kulinka.abonents.fragment.AbonentFragment;
-import by.bsuir.kulinka.abonents.fragment.PlanFragment;
-import by.bsuir.kulinka.abonents.fragment.ServiceFragment;
 import com.bsuir.bottomapp.bar.abonents.databinding.ActivityMainBinding;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import by.bsuir.kulinka.abonents.fragment.AbonentFragment;
+import by.bsuir.kulinka.abonents.fragment.PlanFragment;
+import by.bsuir.kulinka.abonents.fragment.ServiceFragment;
 
 public class MainActivity extends AppCompatActivity
 {
     //----------------------------------------------------------------------------------------------
     //Переменная для биндинга
     ActivityMainBinding binding;
-    List<Fragment> fragmentList;
+
+    //Фрагменты BottomNavigationView
+    final Fragment abonents = new AbonentFragment();
+    final Fragment plans = new PlanFragment();
+    final Fragment services = new ServiceFragment();
+    final FragmentManager fm = getSupportFragmentManager();
+    Fragment active = abonents;
     //----------------------------------------------------------------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -28,37 +39,45 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        //Инициализация списка фрагментов
-        createFragments();
+
         //Настройка BottomBar
         setUpBottomBar();
-        //Установка первого фрагмента
-        getSupportFragmentManager().beginTransaction().add(R.id.main_container, fragmentList.get(0)).commit();
+
+        //Инициализация списка фрагментов
+        createFragments();
     }
     //----------------------------------------------------------------------------------------------
     //Инициализация списка фрагментов
     private void createFragments()
     {
-        fragmentList = new ArrayList<>();
-        fragmentList.add(new AbonentFragment());
-        fragmentList.add(new PlanFragment());
-        fragmentList.add(new ServiceFragment());
+        fm.beginTransaction().add(R.id.main_container, services, "services").hide(services).commit();
+        fm.beginTransaction().add(R.id.main_container, plans, "plans").hide(plans).commit();
+        fm.beginTransaction().add(R.id.main_container, abonents, "abonents").commit();
     }
     //----------------------------------------------------------------------------------------------
     //Настройка BottomBar
     private void setUpBottomBar()
     {
+        //Обработка выбора фрагмента
         binding.bottomBar.setOnItemSelectedListener(i ->
         {
-            loadFragment(i);
+            switch (i)
+            {
+                case 0:
+                    fm.beginTransaction().hide(active).show(abonents).commit();
+                    active = abonents;
+                    return true;
+                case 1:
+                    fm.beginTransaction().hide(active).show(plans).commit();
+                    active = plans;
+                    return true;
+                case 2:
+                    fm.beginTransaction().hide(active).show(services).commit();
+                    active = services;
+                    return true;
+            }
             return true;
         });
-    }
-    //----------------------------------------------------------------------------------------------
-    //Заменить фрагмент в контейнере
-    private void loadFragment(int i)
-    {
-        getSupportFragmentManager().beginTransaction().replace(R.id.main_container, fragmentList.get(i)).commit();
     }
     //----------------------------------------------------------------------------------------------
 }
